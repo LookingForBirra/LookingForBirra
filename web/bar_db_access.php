aa1d88c326224817edea741aca19fbc7c7117511
Daniel <potrayarrick@gmail.com>
2014-03-19 20:19:35 +0100

    Se buscan bares por $_GET en el PHP. Buscador muy básico en HTML.

--- web/bar_db_access.php
+++ web/bar_db_access.php
@@ -1,46 +1,54 @@
 <?php
-  include 'database_connection.php';
+include 'database_connection.php';
 
-  header('Access-Control-Allow-Origin: *');   
-  $connection = connect();
+header('Access-Control-Allow-Origin: *');
+$connection = connect();
 
-  if(! $connection ){
+if(! $connection ){
     die('Could not connect: ' . mysqli_error());
-  }
+}
 
-  $verb = $_SERVER['REQUEST_METHOD'];
-  $resultado;
-  $rows = array();
+$verb = $_SERVER['REQUEST_METHOD'];
+$resultado;
+$rows = array();
 
-  switch($verb){
+switch($verb){
     case 'GET':
-      if($_GET['id_BAR'] != null){
-        $resultado = mysqli_query($connection, "SELECT * FROM BAR WHERE id_BAR = '" . $_GET['id_BAR']."'") or die('Could not query: ' . mysqli_error($connection));
-        print json_encode(mysqli_fetch_assoc($resultado));
-      }else{
-        $resultado = mysqli_query($connection, "SELECT * FROM BAR;") or die('Could not query: ' . mysqli_error($connection));
-        while($r = mysqli_fetch_assoc($resultado)) {
-          $rows[] = $r;
+        if($_GET['id_BAR'] != null){
+            $resultado = mysqli_query($connection, "SELECT * FROM BAR WHERE id_BAR = '" . $_GET['id_BAR']."'") or die('Could not query: ' . mysqli_error($connection));
+            print json_encode(mysqli_fetch_assoc($resultado));
+        }else{
+            $tipoBusqueda = $_GET['tipoBusqueda'];
+            $valorBusqueda = $_GET['valorBusqueda'];
+            if ($tipoBusqueda != null && $valorBusqueda != null){
+                $query = "SELECT * FROM BAR WHERE ".$tipoBusqueda." like '".$valorBusqueda."'";
+                $resultado = mysqli_query($connection, $query) or die('Could not query: ' . mysqli_error($connection));
+            }
+            else
+                $resultado = mysqli_query($connection, "SELECT * FROM BAR;") or die('Could not query: ' . mysqli_error($connection));
+            while($r = mysqli_fetch_assoc($resultado)) {
+                $rows[] = $r;
+            }
+            print json_encode($rows);
+
         }
-        print json_encode($rows);
-      }
-      
-      break;
+
+        break;
 
     case 'POST':
-      if($_POST['id_CERVEZA'] != null){
-        $resultado = mysqli_query($connection, "UPDATE BAR SET Imagen='".$_POST['imagen']."' WHERE id_BAR='".$_POST['id_BAR']."'")or die('Could not query: ' . mysqli_error($connection));
-      }else{
-        $resultado = mysqli_query($connection, "INSERT INTO BAR(Nombre, Direccion, Tipo, Aforo, Link, Tematica, Zona) VALUES('".$_POST['name']."', '".$_POST['address']."', ".$_POST['type'].", ".$_POST['crew'].", '".$_POST['link']."', ".$_POST['theme'].", '".$_POST['zone']."')")or die('Could not query: ' . mysqli_error($connection));
-      }
-      break;
+        if($_POST['id_CERVEZA'] != null){
+            $resultado = mysqli_query($connection, "UPDATE BAR SET Imagen='".$_POST['imagen']."' WHERE id_BAR='".$_POST['id_BAR']."'")or die('Could not query: ' . mysqli_error($connection));
+        }else{
+            $resultado = mysqli_query($connection, "INSERT INTO BAR(Nombre, Direccion, Tipo, Aforo, Link, Tematica, Zona) VALUES('".$_POST['name']."', '".$_POST['address']."', ".$_POST['type'].", ".$_POST['crew'].", '".$_POST['link']."', ".$_POST['theme'].", '".$_POST['zone']."')")or die('Could not query: ' . mysqli_error($connection));
+        }
+        break;
 
     case 'PUT':
-      break;
+        break;
 
     default:
-      break;
-  }
+        break;
+}
 
-  disconnect();
+disconnect();
 ?>
