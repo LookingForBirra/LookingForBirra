025432ac79b72a6216fd23b23055c8830f7a3ac3
Alberto Casares <rs.emud@gmail.com>
2014-03-20 12:02:18 +0100

    Actualizado BarController para implementar busqueda

--- lib/bar.dart
+++ lib/bar.dart
@@ -4,7 +4,7 @@ class Bar{
   int id;
   String name;
   String address;
-  int tipo;
+  String tipo;
   int aforo;
   String link;
   String tematica;
@@ -14,24 +14,6 @@ class Bar{
   
   Bar(this.id, this.name, this.address, this.tipo ,this.aforo ,this.link, this.tematica,
       this.zona ,this.imgURL);
-  
-  String get tipoAsString{
-    switch(this.tipo){
-          case 1:
-            return "Bar";
-            break;
-          case 2:
-            return "Pub";
-            break;
-          case 3:
-            return "Taberna";
-            break;
-          case 4:
-            return "Terraza";
-            break;
-          default: return "";
-        }
-  }
   
   Map<String, dynamic> toJson() => <String, dynamic>{
       "id_BAR": id,
@@ -46,6 +28,6 @@ class Bar{
   };
 
   Bar.fromJson(Map<String, dynamic> json) : this(int.parse(json['id_BAR']),
-      json['Nombre'], json['Direccion'], int.parse(json['Tipo']), int.parse(json['Aforo']),
+      json['Nombre'], json['Direccion'], json['Tipo'], int.parse(json['Aforo']),
       json['Link'], json['Tematica'], json['Zona'], json['Imagen']);
 }
--- lib/controller/barcontroller.dart
+++ lib/controller/barcontroller.dart
@@ -1,26 +1,6 @@
 import 'package:angular/angular.dart';
-import '../bar.dart';
+import 'package:LookingForBirra/bar.dart';
 
-@NgController(
-    selector: '[bar]',
-    publishAs: 'ctrl')
-class BarController {
-  Bar currentBar = new Bar(1, "Casa paco", "Aqui al lao", 2, 2, "", "", "", "");
-  Http _http;
-  
-  BarController() {
-  }
-  
-  String get barName => currentBar.name;
-
-  void selectBar(int id) {
-    currentBar = _loadBar(id);
-  }
-
-  Bar _loadBar(int id) {
-    return null;
-  }
-}
 
 @NgController(
     selector: '[bar-list]',
@@ -31,6 +11,19 @@ class BarListController {
   
   BarListController(Http this._http) {
     _loadData();
+  }
+  
+  void _search(String tipo, String valor){
+    barList = new List<Bar>();
+    var url = 'http://lookingforbirra.esy.es/bar_db_access.php';
+    Map args = { 'tipoBusqueda' : tipo,
+                 'valorBusqueda' : valor};
+    _http.get(url).then((HttpResponse response){
+      print(response);
+      for(Map m in response.data){
+        barList.add(new Bar.fromJson(m));
+      }
+    });
   }
   
   void _loadData() {
